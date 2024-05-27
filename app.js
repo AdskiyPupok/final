969e1f85e91c46b756fee821619595bdc7226491
AdskiyPupok <150174912+AdskiyPupok@users.noreply.github.com>
2024-05-27 14:04:22 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+<<<<<<< HEAD
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const connectToDatabase = require('./database/connect');
+const cors = require('./middlewares/cors');
+const apiRouter = require('./routes/api');
+const app = express();
+const pagesRouter = require("./routes/pages")
+
+
+const PORT = 3001;
+
+
+const cookieParser = require("cookie-parser");
+
+connectToDatabase();
+app.use(
+  cors,
+  cookieParser(),
+  bodyParser.json(),
+  pagesRouter, 
+  apiRouter,
+  express.static(path.join(__dirname, "public"))
+);
+
+app.listen(PORT);
+=======
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const connectToDatabase = require('./database/connect');
+const cors = require('./middlewares/cors');
+const apiRouter = require('./routes/api');
+const app = express();
+const pagesRouter = require("./routes/pages")
+const PORT = 3000;
+
+connectToDatabase();
+
+const cookieParser = require("cookie-parser");
+
+
+app.use(
+  cors,
+  cookieParser(),
+  bodyParser.json(),
+  pagesRouter, // Добавляем роутер для страниц
+  apiRouter,
+  express.static(path.join(__dirname, "public"))
+);
+
+// Остальной код
+
+
+// Запуск приложения
+
+app.listen(PORT);
+>>>>>>> ff2f5a6541f671cd1bc70d1034bdb760eb27561a
